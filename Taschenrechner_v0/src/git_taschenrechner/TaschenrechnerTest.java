4e76cfda0b303de6283c7673889b788390d152d3
user <user@192.168.34.84>
2026-09-03 10:55:23 +0200

    Menu erweitert und cases hinzugefuegt

--- Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
@@ -11,16 +11,19 @@ public class TaschenrechnerTest {
 		Taschenrechner ts = new Taschenrechner();
 
 		int swValue;
+		double zahl1;
+		double zahl2;
 
 		// Display menu graphics
 		System.out.println("============================");
-		System.out.println("|   MENU SELECTION DEMO    |");
+		System.out.println("|      MENU SELECTION      |");
 		System.out.println("============================");
 		System.out.println("| Options:                 |");
 		System.out.println("|        1. Addieren       |");
 		System.out.println("|        2. Subtrahieren   |");
-		System.out.println("|        3. Dividieren     |");
-		System.out.println("|        4. Exit           |");
+		System.out.println("|        3. Multiplizieren |");
+		System.out.println("|        4. Dividieren     |");
+		System.out.println("|        5. Exit           |");
 		System.out.println("============================");
 		System.out.print(" Select option: ");
 		swValue = myScanner.next().charAt(0);
@@ -28,13 +31,33 @@ public class TaschenrechnerTest {
 		// Switch construct
 		switch (swValue) {
 		case '1':
-			System.out.println("4 + 5 = " + ts.add(4, 5));
+			System.out.println("Bitte ersten Summand eingeben: ");
+	        zahl1 = myScanner.nextDouble();
+	        System.out.println("Bitte zweiten Summand eingeben: ");
+	        zahl2 = myScanner.nextDouble();
+			System.out.println(zahl1 + " + " + zahl2 + " = " + ts.add(zahl1, zahl2));
 			break;
-			
-			
-		  //  add your code here
-		  
-		  
+		case '2':
+			System.out.println("Bitte Minuend eingeben: ");
+	        zahl1 = myScanner.nextDouble();
+	        System.out.println("Bitte Subtrahend eingeben: ");
+	        zahl2 = myScanner.nextDouble();
+			System.out.println(zahl1 + " - " + zahl2 + " = " + ts.sub(zahl1, zahl2));
+			break;
+		case '3':
+			System.out.println("Bitte ersten Faktor eingeben: ");
+	        zahl1 = myScanner.nextDouble();
+	        System.out.println("Bitte zweiten Faktor eingeben: ");
+	        zahl2 = myScanner.nextDouble();
+			System.out.println(zahl1 + " * " + zahl2 + " = " + ts.mul(zahl1, zahl2));
+			break;
+		case '4':
+			System.out.println("Bitte Dividend eingeben: ");
+	        zahl1 = myScanner.nextDouble();
+	        System.out.println("Bitte Divisor eingeben: ");
+	        zahl2 = myScanner.nextDouble();
+			System.out.println(zahl1 + " / " + zahl2 + " = " + ts.div(zahl1, zahl2));
+			break;
 		default:
 			System.out.println("Invalid selection");
 			break; // This break is not really necessary
